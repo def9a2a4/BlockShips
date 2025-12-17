f5f928ddefee59d07575934e0b697bfee3fcf170
def9a2a4 <def9a2a4@d3420b8b7fe04.name>
2025-12-16 19:33:46 -0800

    fix ProtocolLib dep
    see https://github.com/dmulloy2/ProtocolLib/issues/3528

--- blockships/build.gradle.kts
+++ blockships/build.gradle.kts
@@ -17,15 +17,11 @@ repositories {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
-    maven {
-        name = "dmulloy2-repo"
-        url = uri("https://repo.dmulloy2.net/repository/public/")
-    }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.3.0")
+    compileOnly("net.dmulloy2:ProtocolLib:5.3.0")
 }
 
 tasks {
